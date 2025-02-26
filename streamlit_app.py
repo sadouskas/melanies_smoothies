50780c6079c4690b5cc08e3725e778dbb2aaf643
sadouskas <as288103@ohio.edu>
2025-02-26 09:53:49 -0500

    ST

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
- Smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
- st.text(smoothiefroot_response)
+    import requests
+    Smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
 
